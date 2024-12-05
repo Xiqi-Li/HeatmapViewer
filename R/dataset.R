c5f4fef2444c43d840678193cd6a7a4c24bfca3f
Xiqi-Li <58788902+Xiqi-Li@users.noreply.github.com>
2024-12-05 14:37:40 -0600

    moved data

--- R/dataset.R
+++ R/dataset.R
@@ -3,5 +3,24 @@
 #'
 #' @name protein_coding_ensemble2symbol
 #' @docType data
-#' @keywords data
-NULL
+#' @keywords datasets
+#' @usage data(protein_coding_ensemble2symbol)
+
+#' concensus_Census
+#'
+#' @name concensus
+#' @docType data
+#' @keywords datasets
+#' @usage data(concensus)
+#' @description
+#' concensus_Census_allWed_Jan_18_20_33_56_2023
+#'
+
+#' cancerGeneList_oncoKB_lastupdate05192023
+#'
+#' @name cancerGeneList_oncoKB
+#' @docType data
+#' @keywords datasets
+#' @usage data(cancerGeneList)
+#' @description
+#' cancerGeneList_oncoKB_lastupdate05192023
